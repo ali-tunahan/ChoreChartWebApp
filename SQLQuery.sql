36025af34005811935cd734ee6949405216ac645
Ali Tunahan Işık <tunahan0735@gmail.com>
2022-07-26 21:28:37 +0300

    SQLQuery added to easily create a simple database for the project.

---
+++ SQLQuery.sql
@@ -0,0 +1,13 @@
+﻿CREATE DATABASE ChoreChartDatabase
+
+GO
+USE ChoreChartDatabase
+CREATE TABLE [dbo].[Chores] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (50) NULL,
+    [Description] NVARCHAR (50) NULL,
+    [Priority]    INT           NULL,
+    [IsDone]      INT           NULL,
+    [DateToDo]    DATETIME      NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
